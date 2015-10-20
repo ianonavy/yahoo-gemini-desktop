c470e5008acee824c56a0c4a583080a344339254
Ian Adam Naval <ianonavy@gmail.com>
2015-10-19 21:36:09 -0400

    Add initial boilerplate

---
+++ component/main/Main.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var Main = React.createClass({
+  displayName: 'Main',
+  render: function() {
+    return (
+      <div>
+        <h1>Yahoo Gemini Desktop</h1>
+        <p>Welcome to Yahoo Gemini Desktop. This is just some boilerplate.</p>
+      </div>
+    );
+  },
+});
+
+module.exports = Main;
